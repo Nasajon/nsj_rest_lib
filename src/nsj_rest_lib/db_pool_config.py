315bb40ddb99f48b018345912963e3d5bd012ddc
Hugo Rabock <hugorabock@nasajon.com.br>
2024-03-07 19:36:38 -0300

    Alterando criacao de string de conexao para permitir senhas com caracteres especiais

--- src/nsj_rest_lib/db_pool_config.py
+++ src/nsj_rest_lib/db_pool_config.py
@@ -10,6 +10,7 @@ from nsj_rest_lib.settings import DB_POOL_SIZE
 
 import sqlalchemy
 
+from sqlalchemy.engine import URL
 
 def create_pool(database_conn_url):
     # Creating database connection pool
@@ -25,13 +26,26 @@ def create_pool(database_conn_url):
 
 
 if DATABASE_DRIVER.upper() in ["SINGLE_STORE", "MYSQL"]:
-    database_conn_url = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASS}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+    database_conn_url = URL.create(
+                            "mysql+pymysql", 
+                            username=DATABASE_USER,
+                            password=DATABASE_PASS,
+                            host=DATABASE_HOST, 
+                            database=DATABASE_NAME,
+                            port=DATABASE_PORT,
+                        )  
 else:
     if ENV.upper() == "GCP":
         database_conn_url = f"postgresql+pg8000://{DATABASE_USER}:{DATABASE_PASS}@/{DATABASE_NAME}?unix_sock=/cloudsql/{CLOUD_SQL_CONN_NAME}/.s.PGSQL.{DATABASE_PORT}"
     else:
-        database_conn_url = f"postgresql+pg8000://{DATABASE_USER}:{DATABASE_PASS}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
-
+        database_conn_url = URL.create(
+                                "postgresql+pg8000", 
+                                username=DATABASE_USER,
+                                password=DATABASE_PASS,
+                                host=DATABASE_HOST, 
+                                database=DATABASE_NAME,
+                                port=DATABASE_PORT,
+                            )            
 
 def default_create_pool():
     return create_pool(database_conn_url)
