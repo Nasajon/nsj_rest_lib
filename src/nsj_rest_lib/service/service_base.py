3d0c18119f3e8d3463b8d93e27e819655a7be831
Keyson Mota <keysonmota@nasajon.com.br>
2024-11-22 10:22:02 -0300

    Suporte a receber uma lista de objetos no método POST

--- src/nsj_rest_lib/service/service_base.py
+++ src/nsj_rest_lib/service/service_base.py
@@ -742,6 +742,41 @@ class ServiceBase:
             custom_after_insert=custom_after_insert,
         )
 
+    def insert_list(
+        self,
+        dtos: List[DTOBase],
+        aditional_filters: Dict[str, Any] = None,
+        custom_before_insert: Callable = None,
+        custom_after_insert: Callable = None,
+    ) -> List[DTOBase]:
+
+        _lst_return = []
+        try:
+            self._dao.begin()
+
+            for dto in dtos:
+                _return_object = self._save(
+                    insert=True,
+                    dto=dto,
+                    manage_transaction=False,
+                    partial_update=False,
+                    aditional_filters=aditional_filters,
+                    custom_before_insert=custom_before_insert,
+                    custom_after_insert=custom_after_insert,
+                )
+
+                if _return_object is not None:
+                    _lst_return.append(_return_object)
+
+        except:
+            self._dao.rollback()
+            raise
+        finally:
+            self._dao.commit()
+
+        return _lst_return
+
+
     def update(
         self,
         dto: DTOBase,
@@ -760,6 +795,42 @@ class ServiceBase:
             custom_before_update=custom_before_update,
             custom_after_update=custom_after_update,
         )
+
+    def update_list(
+        self,
+        dtos: List[DTOBase],
+        #id: Any,
+        aditional_filters: Dict[str, Any] = None,
+        custom_before_update: Callable = None,
+        custom_after_update: Callable = None
+    ) -> List[DTOBase]:
+
+        _lst_return = []
+        try:
+            self._dao.begin()
+
+            for dto in dtos:
+                _return_object = self._save(
+                    insert=False,
+                    dto=dto,
+                    manage_transaction=True,
+                    partial_update=False,
+                    #id=id,
+                    aditional_filters=aditional_filters,
+                    custom_before_update=custom_before_update,
+                    custom_after_update=custom_after_update,
+                )
+
+                if _return_object is not None:
+                    _lst_return.append(_return_object)
+
+        except:
+            self._dao.rollback()
+            raise
+        finally:
+            self._dao.commit()
+
+        return _lst_return
 
     def partial_update(
         self,
